2f01af813413c2b4b208599eebebf553c14b6fd1
Shital Chaudhary <shitalchau10@gmail.com>
2021-07-19 19:33:42 -0500

    add elogic and error handler to form - js

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,27 @@
+// select entrire form
+var userFormEl = document.querySelector("#user-form");
+// select username input field
+var nameInputEl = document.querySelector("#username");
+
+
+var formSubmitHandler = function(event){
+    // stop reloading by default
+    event.preventDefault();
+    console.log(event);
+
+    // get value from form field
+    var username = nameInputEl.value.trim();
+
+    // handle error on form
+    if(username){
+        getUserRepos(username);
+        nameInputEl.value = "";
+    }else{
+        alert("Please enter a GitHub username");
+    }
+}
+userFormEl.addEventListener("submit", formSubmitHandler);
+
 var getUserRepos = function (user) {
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
